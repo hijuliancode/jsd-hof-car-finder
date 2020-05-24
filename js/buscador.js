68f096f893c4f3b2fbdb31c75e08d708982404eb
JulianCode <hijuliansosa@protonmail.com>
2020-05-24 17:48:46 -0500

    Selecting Form Elements

--- js/buscador.js
+++ js/buscador.js
@@ -12,21 +12,50 @@ for(let i = max; i >  min; i--) {
     document.querySelector('#year').appendChild(option);
 }
 
-function obtenerAutos() {
-    return autos
+// Variables
+const marca = document.querySelector('#marca'),
+      year = document.querySelector('#year'),
+      minimo = document.querySelector('#minimo'),
+      maximo = document.querySelector('#maximo'),
+      transmision = document.querySelector('#transmision'),
+      puertas = document.querySelector('#puertas'),
+      color = document.querySelector('#color');
+
+// Datos para la busqueda
+let datosBusqueda = {
+    marca: '',
+    year: '',
+    minimo: '',
+    maximo: '',
+    puertas: '',
+    transmision: '',
+    color: ''
 }
 
 // Event Listeners
 document.addEventListener('DOMContentLoaded', () => mostrarAutos(autos));
+marca.addEventListener('input', (e) => {
+    // const selected = e.target.options[e.target.options.selectedIndex].value
+    datosBusqueda.marca = e.target.value
+    // Llamar función de filtrar autos
+    filtrarAutos()
+})
+
+// Functions
+function obtenerAutos() {
+    return autos
+}
 
 function mostrarAutos(autos) {
     const contenedor = document.querySelector('#resultado')
     autos.forEach(auto => {
-        console.log(auto)
+        // console.log(auto)
         const autoHTML = document.createElement('p')
-        autoHTML.innerHTML = `
-            ${auto.marca} ${auto.modelo} ${auto.color} - ${auto.year} - ${auto.puertas} puertas - Transmisión: ${auto.transmision} - Precio: $${auto.precio}
-        `
+        autoHTML.innerHTML = `${auto.marca} ${auto.modelo} ${auto.color} - ${auto.year} - ${auto.puertas} puertas - Transmisión: ${auto.transmision} - Precio: $${auto.precio}`
         contenedor.appendChild(autoHTML)
     });
+}
+
+function filtrarAutos() {
+    console.log('Filtrar Autos')
 }
